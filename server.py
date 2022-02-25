221a883ca4776c60b515b50db86587fb077a9a07
Michal Dobeš <dobesmic@fit.cvut.cz>
2022-02-25 21:49:33 +0100

    Document all methods using Doxygen

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
+
+
+## TCP/IP multithreaded server for BI-PSI homework.
+#
+# Main objective is to reach zero coordinates with client (robot). Sends requests for turning and moving the robot.
+# At the beginning of communication the server authenticates the client.
+# The server can deal with client halting (robot recharging) and very simple algorithm to avoid obstacles on robots path.
+
+from platform import python_branch
 import socket
 import sys
 import threading
 import enum
 
+# Global variables defined by server specification
 HOST = '127.0.0.1'
 port = None
 
@@ -25,21 +35,43 @@ MESSAGES = {
     "SERVER_PICK_UP": "105 GET MESSAGE\a\b".encode("ascii")
 };
 
+
+## Class implementing all of server logic after communication has been initialized
+#
 class ServerThread(threading.Thread):
 
+    ## Class for processing client authentication
+    #
     class Authentication():
 
+        ## Defines phases of authentication protocol as enum
+        #
         class AuthenticationPhase(enum.Enum):
             USERNAME = 0
             KEY_ID = 1
             CONFIRMATION = 2
             AUTHENTICATED = 3
 
+        ## Constructor
+        #
+        #  @param self
+        #  @param connection Connection to client
+        #
         def __init__(self, connection) -> None:
             self.connection = connection
             self.phase = self.AuthenticationPhase.USERNAME
 
-        def length_valid(self, data) -> bool:
+        ## Check if message length is valid before processing it
+        #
+        #  Calculates whether the message fits the expected length.
+        #
+        #  @param self
+        #  @param data Received message
+        #  
+        #  @returns bool Valid length
+        #
+        def verify_length(self, data) -> bool:
+            # Check if contains part of message separation characters
             prefix_size = 0
             if len(data) != 0 and data[-1] == "\a":
                 prefix_size += 1
@@ -66,6 +98,10 @@ class ServerThread(threading.Thread):
                     return False
             return True
 
+        ## Calculate hash as expected by server specification
+        #
+        #  @param self
+        #
         def calculate_hash(self):
             self.hash = 0
             for c in self.username:
@@ -73,16 +109,25 @@ class ServerThread(threading.Thread):
             self.hash *= 1000
             self.hash %= 65536
 
+        ## Process received authentication message
+        #
+        #  @param self
+        #  @param data Received message
+        #
+        #  @returns bool If true authentication is valid, else should terminate connection
+        #
         def authenticate(self, data) -> bool:
 
             global SERVER_KEY
             global CLIENT_KEY
             global MESSAGES
 
-            if not self.length_valid(data):
+            # Check for correct length
+            if not self.verify_length(data):
                 self.connection.send(MESSAGES["SERVER_SYNTAX_ERROR"])
                 return False
 
+            # Perform required action based on authetication phase defined by server specification
             if self.phase == self.AuthenticationPhase.USERNAME:
                 self.username = data
 
@@ -130,14 +175,23 @@ class ServerThread(threading.Thread):
                 self.connection.send(MESSAGES["SERVER_TURN_LEFT"])
                 return True     
 
+    ## Class for processing client movement
+    #
     class Movement():
 
+        ## Defines direction of robot movement
+        #
         class Direction(enum.Enum):
             POSITIVE_X = 0
             POSITIVE_Y = 1
             NEGATIVE_X = 2
             NEGATIVE_Y = 3
 
+        ## Constructor
+        #
+        #  @param self
+        #  @param connection Connection to client
+        #
         def __init__(self, connection) -> None:
             self.connection = connection
             self.x = None
@@ -149,11 +203,22 @@ class ServerThread(threading.Thread):
             self.first_move = True
             self.unstuck_moves_left = 0
 
+        ## Sends message requesting client to move
+        #
+        #  @param self
+        #
         def move(self):
             global MESSAGES
             self.connection.send(MESSAGES["SERVER_MOVE"])
             self.last_moved = True
 
+        ## Sends message requesting client to rotate
+        #
+        #  Calculates new direction the robot will be facing and sends request
+        #
+        #  @param self
+        #  @param left If true, rotate anticlockwise, else rotate clockwise
+        #
         def rotate(self, left):
             global MESSAGES
             rotation_value = None
@@ -169,6 +234,13 @@ class ServerThread(threading.Thread):
                 self.direction = self.Direction(rotation_value)
             self.last_moved = False
 
+        ## Calculates if next request should be rotation or movement
+        #
+        #  @param self
+        #  @param direction Requested direction of movement
+        #  
+        #  If facing requested direction of movement, move, else rotate whichever way is closer.
+        #
         def calculate_direction(self, direction):
             if not self.direction or self.direction == direction:
                 self.move()
@@ -178,8 +250,16 @@ class ServerThread(threading.Thread):
                 else:
                     self.rotate(True)
 
+        ## If facing obstacle, moves to the said of given obstacle
+        #
+        #  @param self
+        #
+        #  Calculates more advantageous side of obstacle to move to.
+        #
         def unstuck(self):
             left = True
+
+            # Calculate which side of obstacle to move to
             if self.direction:
                 if self.x > 0:
                     if self.y < 0:
@@ -188,17 +268,28 @@ class ServerThread(threading.Thread):
                     if self.y > 0:
                         left = False
 
+            # Move
             self.rotate(left)
             self.move()
             self.rotate(not left)
             self.move()
+
+            # @var unstuck_moves_left Sets amount of calls to ignore (because of unstuck mechanism)
             self.unstuck_moves_left = 4
 
+        ## Shortcut to request picking up message at zero coords (goal of client)
+        #
+        #  @param self
+        #
         def get_message(self):
             global MESSAGES
             self.picking_up_message = True
             self.connection.send(MESSAGES["SERVER_PICK_UP"])
 
+        ## Calculates next move to zero coords based on current coords
+        #
+        #  @param self
+        #
         def calculate_move(self):
             if self.x != 0:
                 if self.x > 0:
@@ -294,6 +385,7 @@ class ServerThread(threading.Thread):
                 self.x = new_x
                 self.y = new_y
 
+                # Check if first move or processing stuck mechanism messages or if client robot is in final destination and move accordingly
                 if self.first_move:
                     self.first_move = False
                     if self.x == 0 and self.y == 0:
@@ -314,6 +406,12 @@ class ServerThread(threading.Thread):
                 return False
             return True
 
+    ## Constructor
+    #
+    #  @param self
+    #  @param connection Connection to client
+    #  @param address Tuple of IPv4 address and port of connected client
+    #
     def __init__(self, connection, address) -> None:
         threading.Thread.__init__(self)
         self.connection = connection
@@ -324,20 +422,31 @@ class ServerThread(threading.Thread):
         self.active = True
         self.recharging = False
 
-        self.connection.settimeout(1)
+        self.connection.settimeout(1) #Sets default timeout
 
         print("OK: Connected from " + address[0] + ":" + str(address[1]))
 
+    ## Shortcut for sending sytax error message
+    #
     def syntax_error(self):
         global MESSAGES
         self.connection.send(MESSAGES["SERVER_SYNTAX_ERROR"])
         self.active = False
 
+    ## Shortcut for sending sytax logic message
+    #
     def logical_error(self):
         global MESSAGES
         self.connection.send(MESSAGES["SERVER_LOGIC_ERROR"])
         self.active = False
 
+    ## Process recharge message
+    # 
+    #  Handle recharging (client halting).
+    #  If already recharging set timeout to default, else set timeout to specified value.
+    # 
+    #  @param self
+    #
     def recharge(self):
         if not self.recharging:
             self.connection.settimeout(5)
@@ -346,12 +455,21 @@ class ServerThread(threading.Thread):
             self.connection.settimeout(1)
             self.recharging = False
 
+    ## Extract and process messages in recieved data
+    #
+    #  Looks for separation characters in data and extracts message before these characters.
+    #  First tries to process recharging (halting) message, if not halted sends recieved message to authentication or movement handler in corresponding class based on current status of authentication.
+    #  
+    #  @param self
+    #
+    #  @returns bool If failed and needs to terminate connection false, else true.
     def handle_data(self) -> bool:
         while '\a\b' in self.data:
             new_partition = self.data.partition("\a\b")
             new_string = new_partition[0]
             self.data = new_partition[2]
 
+            # Recharging handling
             if new_string == "RECHARGING":
                 if self.recharging:
                     self.logical_error()
@@ -367,6 +485,7 @@ class ServerThread(threading.Thread):
                         self.logical_error()
                         return False
 
+            # Authentication and movement handling
             if (self.authentication.phase != self.authentication.AuthenticationPhase.AUTHENTICATED):
                 if not self.authentication.authenticate(new_string):
                     self.active = False
@@ -377,14 +496,21 @@ class ServerThread(threading.Thread):
                     return False
         return True
 
+    ## Primary function of thread
+    #
+    #  Receives data from connection and calls handler to process it.
+    #
+    #  If data is longer than specified limit and still not processed, optimizes by cutting connection.
     def run(self):
         while self.active:
+            # Receive data
             try:
                 self.data += self.connection.recv(1024).decode("ascii")
             except:
                 self.active = False
                 self.connection.close()
 
+            # Handle data
             if not self.handle_data():
                 self.connection.close()
 
@@ -397,8 +523,16 @@ class ServerThread(threading.Thread):
                 if not self.movement.verify_length(self.data):
                     self.syntax_error()
                     self.connection.close()
-        
+
+## Get port value from stdio.
+#
+#  Port needs to be between 1024 and 65353 inclusive.
+#
+#  @returns bool Sucess or failiure
+#
+#  Saves port value into global variable PORT.
 def get_port() -> bool:
+
     if (len(sys.argv) <= 1):
         print("ERR: Add port as an argument")
         return False
@@ -416,6 +550,8 @@ def get_port() -> bool:
 
     return True
 
+## Creates server socket, starts listening for connections, when connected creates new thread for serving client.
+#
 def main():
     if (not get_port()):
         return None
@@ -449,6 +585,7 @@ def main():
     except:
         serversocket.close()
         print("OK: Exiting")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
